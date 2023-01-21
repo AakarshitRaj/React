42ac42aee9bb965bdf294a8924819c131131db96
AAKARSHIT001 <86571868+AAKARSHIT001@users.noreply.github.com>
2023-01-21 14:03:33 +0530

    1st

---
+++ src/card.js
@@ -0,0 +1,9 @@
+import React from "react"; 
+
+function card(){
+    return(
+        <div>
+            <h1>my name aakarshit</h1>
+        </div>
+    )
+}
--- src/index.js
+++ src/index.js
@@ -2,16 +2,28 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import card from './card';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const headingstyle={
+    backgroundColor:'yellow',
+}
+const name="raj";
+const myVar=(<div>
+    <h1 style={{backgroundColor:'red'}//border-radius:20px; -> borderRadius:'20px'
+    }className='heading'>hello i am JSX</h1>
+<p style={headingstyle}>aakarshit raj</p>
+{5+4} {name}
+<p className='para'>i am engineer</p>
+
+<card/>;
+</div>)
+const mm=<p>lorem  </p>
+root.render(myVar);
+
+
+
+
+
